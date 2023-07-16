7341a8ee38966b3f237cc4f79b88bb73dac0d910
c1ph3rwolf <c1ph3rwolf@gmail.com>
2023-07-17 02:15:28 +0530

    remove scarecrow from autoinstaller

--- ScareCrow/03-create-undetected-exec-for-win.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "[ ? ] Enter the location where ScareCrow is Located ? : "
-#Read path
-#cd $path
-#cd /usr/tools/ScareCrow
-
-# Start the build
-./ScareCrow -Loader binary -console -domain microsoft.com -I shellcode.bin
